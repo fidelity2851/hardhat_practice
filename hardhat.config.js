914f3bfc1afc433d471f1129fefe2aa96faf5aa7
FranCode <ukwuanifidelis2851@gmail.com>
2025-01-28 08:05:44 +0100

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,13 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.28",
+  networks: {
+    ganache: {
+      url: "HTTP://127.0.0.1:7545",
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};
